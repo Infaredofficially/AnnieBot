0257cf5970337bc82d60c22f809b5d15c48d4edb
Examinati <72595444+Eximinati@users.noreply.github.com>
2023-03-13 00:00:44 +0500

    Update help.js
    will send greeting according to time 👍

--- Commands/Core/help.js
+++ Commands/Core/help.js
@@ -54,7 +54,16 @@ module.exports = {
         };
         const uptime = () => formatTime(process.uptime());
 
-      let textHelpMenu = `Konnichiwa *${pushName}* Senpai,
+       let greeting;
+
+        if (hour >= 0 && hour < 12) {
+          greeting = "Ohayou gozaimasu"; //good morning
+        } else if (hour >= 12 && hour < 18) {
+          greeting = "Konnichiwa"; //good afternoon
+        } else {
+          greeting = "Konbanwa"; //good evening
+        }
+      let textHelpMenu = `${greeting} *${pushName}* Senpai,
 
 I am *${botName}*, a bot developed by *Team Atlas*.
 
