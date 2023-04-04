4d7fcb82822da01522b584f4e99350d111c358d4
Fantox <96835724+FantoX001@users.noreply.github.com>
2023-04-05 01:33:27 +0530

    Update song.js

--- Commands/Media/song.js
+++ Commands/Media/song.js
@@ -10,24 +10,23 @@ module.exports = {
   usage: `play <song name>`,
   react: "🍁",
   start: async (Miku, m, { text, prefix, args }) => {
-    try {
-      if (!args[0])
-        return Miku.sendMessage(
-          m.from,
-          { text: `Please provide a song name to play !` },
-          { quoted: m }
-        );
-      const songSerachTerm = args.join(" ");
-      const songInfo = await yts(songSerachTerm);
-      const song = songInfo.videos[0];
-      let videoUrl = song.url;
-      let videoId = videoUrl.split("v=")[1];
-
-      await Miku.sendMessage(
+    if (!args[0])
+      return Miku.sendMessage(
         m.from,
-        {
-          image: { url: song.thumbnail },
-          caption: `\n*Song name :* _${song.title}_
+        { text: `Please provide a song name to play !` },
+        { quoted: m }
+      );
+    const songSerachTerm = args.join(" ");
+    const songInfo = await yts(songSerachTerm);
+    const song = songInfo.videos[0];
+    let videoUrl = song.url;
+    let videoId = videoUrl.split("v=")[1];
+
+    await Miku.sendMessage(
+      m.from,
+      {
+        image: { url: song.thumbnail },
+        caption: `\n*Song name :* _${song.title}_
 
 *Duration :* _${song.timestamp}_
 
@@ -35,38 +34,37 @@ module.exports = {
 
 
 _*Downloading Song...*_\n\n`,
-        },
-        { quoted: m }
-      );
+      },
+      { quoted: m }
+    );
 
-      yts(videoId).then((result) => {
-        const length = result.seconds;
+    yts(videoId).then((result) => {
+      const length = result.seconds;
 
-        if (length >= 1800) {
-          return m.reply(
-            "Command Rejected! The audio is more than 30 minutes long! "
+      if (length >= 1800) {
+        return m.reply(
+          "Command Rejected! The audio is more than 30 minutes long! "
+        );
+      } else {
+        const ytaud = YT.mp3(videoId).then((file) => {
+          Miku.sendMessage(
+            m.from,
+            {
+              audio: fs.readFileSync(file.path),
+              mimetype: "audio/mpeg",
+            },
+            { quoted: m }
           );
-        } else {
-          const ytaud = YT.mp3(videoId).then((file) => {
-            Miku.sendMessage(
-              m.from,
-              {
-                audio: fs.readFileSync(file.path),
-                mimetype: "audio/mpeg",
-              },
-              { quoted: m }
-            );
-            fs.unlinkSync(file.path);
-          });
-        }
-      });
-    } catch (err) {
+          fs.unlinkSync(file.path);
+        });
+      }
+    }).catch((err) => {
       console.error(err);
       Miku.sendMessage(
         m.from,
         { text: `Failed to play the song: ${err.message}` },
         { quoted: m }
       );
-    }
+    });
   },
 };
