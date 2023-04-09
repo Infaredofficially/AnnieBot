08d75c15e0bd3039ecfa27d68a86c6f2b33bc0d4
Ahmii <119128285+Ahmii-kun@users.noreply.github.com>
2023-04-09 05:33:35 +0500

    Update song.js

--- Commands/Media/song.js
+++ Commands/Media/song.js
@@ -1,70 +1,72 @@
 const YT = require("../../lib/ytdl-core.js");
 const fs = require("fs");
 const yts = require("youtube-yts");
+const fetch = require("node-fetch");
+const { getBuffer } = require("../../lib/myfunc");
+const ffmpeg = require("fluent-ffmpeg");
+const ffmpegPath = require("@ffmpeg-installer/ffmpeg").path;
+ffmpeg.setFfmpegPath(ffmpegPath);
 
 module.exports = {
   name: "play",
-  alias: ["ytplay", "song"],
-  desc: "To play a song from youtube",
+  alias: ["yt"],
+  desc: "To download a song as mp3 from YouTube link",
+  cool: 30,
   category: "Media",
-  usage: `play <song name>`,
+  usage: `ytad <song link>`,
   react: "🍁",
-  start: async (Miku, m, { text, prefix, args }) => {
+  start: async (Miku, m, { text, prefix, args, mime }) => {
     if (!args[0])
       return Miku.sendMessage(
         m.from,
-        { text: `Please provide a song name to play !` },
+        { text: `Please provide a YouTube Video link !` },
         { quoted: m }
       );
-    const songSerachTerm = args.join(" ");
+      const songSerachTerm = args.join(" ");
     const songInfo = await yts(songSerachTerm);
     const song = songInfo.videos[0];
     let videoUrl = song.url;
     let videoId = videoUrl.split("v=")[1];
 
-    await Miku.sendMessage(
-      m.from,
-      {
-        image: { url: song.thumbnail },
-        caption: `\n*Song name :* _${song.title}_
+    yts({ videoId }).then((result) => {
+      YT.mp3(videoId).then((file) => {
+        const inputPath = file.path;
+        const outputPath = inputPath + ".opus";
 
-*Duration :* _${song.timestamp}_
-
-*Url :* _${song.url}_
-
-
-_*Downloading Song...*_\n\n`,
-      },
-      { quoted: m }
-    );
-
-    yts(videoId).then((result) => {
-      const length = result.seconds;
-
-      if (length >= 1800) {
-        return m.reply(
-          "Command Rejected! The audio is more than 30 minutes long! "
-        );
-      } else {
-        const ytaud = YT.mp3(videoId).then((file) => {
-          Miku.sendMessage(
-            m.from,
-            {
-              audio: fs.readFileSync(file.path),
-              mimetype: "audio/mpeg",
-            },
-            { quoted: m }
-          );
-          fs.unlinkSync(file.path);
-        });
-      }
-    }).catch((err) => {
-      console.error(err);
-      Miku.sendMessage(
-        m.from,
-        { text: `Failed to play the song: ${err.message}` },
-        { quoted: m }
-      );
+        ffmpeg(inputPath)
+          .format("opus")
+          .on("error", (err) => {
+            console.error("Error converting to opus:", err);
+          })
+          .on("end", async () => {
+            const thumbnailBuffer = await getBuffer(song.thumbnail);
+            
+            Miku.sendMessage(
+              m.from,
+              {
+                audio: fs.readFileSync(outputPath),
+                mimetype: "audio/ogg; codecs=opus",
+                ptt: true,
+                contextInfo: {
+                  externalAdReply: {
+                    title: song.title.substr(0, 30),
+                    body: song.description.substr(0, 30),
+                    mediaType: 2,
+                    thumbnail: thumbnailBuffer,
+                    mediaUrl: song.url
+                  }
+                }
+              },
+              { quoted: m }
+            );
+            
+            fs.unlinkSync(inputPath);
+            fs.unlinkSync(outputPath);
+          })
+          
+          
+          .save(outputPath);
+      });
     });
   },
 };
