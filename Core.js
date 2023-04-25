2d814721e310d271ab791d04e79a165fd53980af
Shubha Pratim Biswas <shubhapratimbiswas@gmail.com>
2023-04-25 13:00:55 +0530

    Update Core.js

--- Core.js
+++ Core.js
@@ -92,7 +92,7 @@ module.exports = async (Miku, m, commands, chatUpdate, store) => {
       .map((v) => v.replace(/[^0-9]/g, "") + "@s.whatsapp.net")
       .includes(m.sender);
     const isOwner = global.owner.includes(m.sender);
-    global.suppL = "https://cutt.ly/AtlasBotSupport";
+    global.suppL = "https://cutt.ly/AtlasSupportStrict";
 
     const isCmd = body.startsWith(prefix);
     const quoted = m.quoted ? m.quoted : m;
